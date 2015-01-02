d36f01f5c68ea4b3a98574abac261fa505fa2271
Jason Petersen <jpetersen13@gmail.com>
2015-01-02 14:46:42 -0500

    Update footer text

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<footer class="footer">
 			<div class="container">
-				<p class="hidden-xs">&#169; 2015 Amanda Hatzikyriakou. Website design by <a href="http://peterscene.com" target="_blank">Jason Petersen</a>.</p>
-				<p class="visible-xs">&#169; 2015 Amanda Hatzikyriakou.<br />Website design by <a href="http://peterscene.com" target="_blank">Jason Petersen</a>.</p>
+				<p class="hidden-xs">&#169; 2015 Amanda Hatzikyriakou. Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
+				<p class="visible-xs">&#169; 2015 Amanda Hatzikyriakou.<br />Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
 			</div>
 		</footer>
