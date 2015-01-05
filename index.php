f4c6ce1e81ee8c529b5a1b981337f02046f30106
Jason Petersen <jpetersen13@gmail.com>
2015-01-05 11:08:07 -0500

    Addition of carousel to home page
    And removal of old content.

--- index.php
+++ index.php
@@ -15,40 +15,45 @@
 			<p>The work I create is focused around my quiet relationship with outdoor spaces. I am interested in being a part of the simplicity of nature, capturing areas large and small, and presenting an image unpolluted by noise and invasive human involvement.</p>
 			<p>I have spent time living and studying in Florence, Italy and the Mediterranean island country of Cyprus&mdash;both places having influenced my painting a great deal. Visits to other areas of the United States and Europe are featured in my art work as well. Please check back for the latest in my painting travels.</p>
 		</div>
-		<div class="gallery container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-mazotos1-sm.jpg" alt="cyprus-mazotos1">
+		<div class="container">
+		<div id="carousel-home" class="carousel slide" data-ride="carousel" data-interval="6000">
+			<!-- Indicators -->
+			<ol class="carousel-indicators hidden-xs">
+				<li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-home" data-slide-to="1"></li>
+				<li data-target="#carousel-home" data-slide-to="2"></li>
+			</ol>
+		 
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="/images/cyprus-mazotos1-carousel.jpg" alt="...">
+					<div class="carousel-caption">
+						<h3 class="hidden-xs">Mazotos field, Cyprus</h3>
+					</div>
 				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-mazotos2-sm.jpg" alt="cyprus-mazotos2">
+				<div class="item">
+					<img src="/images/cyprus-natl-park1-carousel.jpg" alt="...">
+					<div class="carousel-caption">
+						<h3 class="hidden-xs">Athalassa National Park sunset, Cyprus</h3>
+					</div>
 				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-mazotos3-sm.jpg" alt="cyprus-mazotos3">
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-mazotos4-sm.jpg" alt="cyprus-mazotos4">
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-natl-park3-sm.jpg" alt="cyprus-natl-park3">
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-thalassa1-sm.jpg" alt="cyprus-thalassa1">
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-thalassa2-sm.jpg" alt="cyprus-thalassa2">
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-natl-park2-sm.jpg" alt="cyprus-natl-park2">
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-					<img class="img-thumbnail" src="/images/cyprus-natl-park1-sm.jpg" alt="cyprus-natl-park1">
+				<div class="item">
+					<img src="/images/cyprus-thalassa1-carousel.jpg" alt="...">
+					<div class="carousel-caption">
+						<h3 class="hidden-xs">Aphrodite's Rock, Cyprus</h3>
+					</div>
 				</div>
 			</div>
+		 
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			</a>
+			<a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div><!-- Carousel -->
 		</div>
 		<div class="spacer20"></div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
