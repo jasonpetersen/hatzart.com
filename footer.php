2f69a942c1b36b419247a9b5da3bd3b60a3758d2
Jason Petersen <jpetersen13@gmail.com>
2015-01-05 10:54:54 -0500

    Update to footer
    Does not need to be wrapped in a container.

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
 		<footer class="footer">
-			<div class="container">
-				<p class="hidden-xs">&#169; 2015 Amanda Hatzikyriakou. Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
-				<p class="visible-xs">&#169; 2015 Amanda Hatzikyriakou.<br />Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
-			</div>
+			<p class="hidden-xs">&#169; 2015 Amanda Hatzikyriakou. Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
+			<p class="visible-xs">&#169; 2015 Amanda Hatzikyriakou.<br />Website design by <a href="http://peterscene.com" target="_blank">Peterscene</a>.</p>
 		</footer>
