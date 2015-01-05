dd57f251dbdaccac02e2b7cf95504878d6364e6a
Jason Petersen <jpetersen13@gmail.com>
2015-01-05 12:50:57 -0500

    Add picture to About page

--- about/index.php
+++ about/index.php
@@ -12,7 +12,11 @@
 	<body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/nav-banner.php'; ?>
 		<div class="about-me container">
-			<p>Amanda Hatzikyriakou has a Bachelor of Fine Arts degree from Arcadia University with a concentration in painting. She also holds a K-12 art education certificate and has 7 years teaching experience in Pennsylvania and New Jersey public schools. She has worked with students of all grades and ability levels. The artist's areas of expertise include landscape, still-life, and portraiture, and she is well-accomplished at drawing and painting techniques in a variety of mediums, as well as crafts and mixed media. She is an active member of the Arts Guild of New Jersey and the Maplewood Artist Collective. You can find her offering private one-on-one art instruction at Renaissance Arts Center in Millburn, New Jersey.</p>
+			<span class="img-with-wrap-left">
+				<img src="/images/ah-about-pic-square.jpg" alt="AH" class="img-circle" width="150" height="150">
+			</span>
+			<p>Amanda Hatzikyriakou has a Bachelor of Fine Arts degree from Arcadia University with a concentration in painting. She also holds a K-12 art education certificate and has 7 years teaching experience in Pennsylvania and New Jersey public schools. She has worked with students of all grades and ability levels.</p>
+			<p>The artist's areas of expertise include landscape, still-life, and portraiture, and she is well-accomplished at drawing and painting techniques in a variety of mediums, as well as crafts and mixed media. She is an active member of the Arts Guild of New Jersey and the Maplewood Artist Collective. You can find her offering private one-on-one art instruction at Renaissance Arts Center in Millburn, New Jersey.</p>
 		</div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
 	</body> 
