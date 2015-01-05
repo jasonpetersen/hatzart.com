194e99ccfc08382be6f266e2d37d47d51018f167
Jason Petersen <jpetersen13@gmail.com>
2015-01-05 17:04:25 -0500

    Indentation
    Keeping it clean!

--- index.php
+++ index.php
@@ -16,44 +16,44 @@
 			<p>I have spent time living and studying in Florence, Italy and the Mediterranean island country of Cyprus&mdash;both places having influenced my painting a great deal. Visits to other areas of the United States and Europe are featured in my art work as well. Please check back for the latest in my painting travels.</p>
 		</div>
 		<div class="container">
-		<div id="carousel-home" class="carousel slide" data-ride="carousel" data-interval="6000">
-			<!-- Indicators -->
-			<ol class="carousel-indicators hidden-xs">
-				<li data-target="#carousel-home" data-slide-to="0" class="active"></li>
-				<li data-target="#carousel-home" data-slide-to="1"></li>
-				<li data-target="#carousel-home" data-slide-to="2"></li>
-			</ol>
-		 
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner">
-				<div class="item active">
-					<img src="/images/cyprus-mazotos1-carousel.jpg" alt="...">
-					<div class="carousel-caption">
-						<h3 class="hidden-xs">Mazotos field, Cyprus</h3>
+			<div id="carousel-home" class="carousel slide" data-ride="carousel" data-interval="6000">
+				<!-- Indicators -->
+				<ol class="carousel-indicators hidden-xs">
+					<li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-home" data-slide-to="1"></li>
+					<li data-target="#carousel-home" data-slide-to="2"></li>
+				</ol>
+			 
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="/images/cyprus-mazotos1-carousel.jpg" alt="...">
+						<div class="carousel-caption">
+							<h3 class="hidden-xs">Mazotos field, Cyprus</h3>
+						</div>
+					</div>
+					<div class="item">
+						<img src="/images/cyprus-natl-park1-carousel.jpg" alt="...">
+						<div class="carousel-caption">
+							<h3 class="hidden-xs">Athalassa National Park sunset, Cyprus</h3>
+						</div>
+					</div>
+					<div class="item">
+						<img src="/images/cyprus-thalassa1-carousel.jpg" alt="...">
+						<div class="carousel-caption">
+							<h3 class="hidden-xs">Aphrodite's Rock, Cyprus</h3>
+						</div>
 					</div>
 				</div>
-				<div class="item">
-					<img src="/images/cyprus-natl-park1-carousel.jpg" alt="...">
-					<div class="carousel-caption">
-						<h3 class="hidden-xs">Athalassa National Park sunset, Cyprus</h3>
-					</div>
-				</div>
-				<div class="item">
-					<img src="/images/cyprus-thalassa1-carousel.jpg" alt="...">
-					<div class="carousel-caption">
-						<h3 class="hidden-xs">Aphrodite's Rock, Cyprus</h3>
-					</div>
-				</div>
-			</div>
-		 
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left"></span>
-			</a>
-			<a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right"></span>
-			</a>
-		</div><!-- Carousel -->
+			 
+				<!-- Controls -->
+				<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				</a>
+				<a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+			</div><!-- Carousel -->
 		</div>
 		<div class="spacer20"></div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
